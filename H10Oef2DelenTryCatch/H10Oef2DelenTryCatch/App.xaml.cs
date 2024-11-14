e4026bf4dd569c6294d829031647b3f6aa7eb289
Indy Iri <indy.iri@student.pxl.be>
2024-11-14 11:04:07 +0100

    wpf aangemaakt

---
+++ H10Oef2DelenTryCatch/H10Oef2DelenTryCatch/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H10Oef2DelenTryCatch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
